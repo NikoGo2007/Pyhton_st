86f77d350955593c9cdccd1d4ae5562a1f749fda
NikoGo2007 <kkurrma2@gmail.com>
2024-10-15 20:34:04 +0300

    add

--- test.py
+++
@@ -1,8 +0,0 @@
-# f = [open('numbers_in_words.txt', 'r').read()]
-# print(f)
-from num2words import num2words
-
-with open("numbers_in_words.txt", "w", encoding="utf-8") as f:
-    for num in range(0, 10001):
-        num_words = num2words(num, lang='ru')
-        f.write(f"{num_words}\n")
