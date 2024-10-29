cb667964d53a02b1bed966d2461d6d2d8c9ededf
NikoGo2007 <kkurrma2@gmail.com>
2024-10-29 16:47:14 +0300

    add

--- main.py
+++ main.py
@@ -121,21 +121,21 @@ def i_10(n):
 #print(i_10(input("строка ")))
 
 
-def i_11(x_1={}, y_1={}, x_2={}, x_3=0, key=0):
-    if key == 1:
+def i_11(x_1={}, y_1={}, x_2={},key=""):
+    if key == "а":
         z = {}
         # z = {**x, **y} способ в котором при повторении ключа будет браться значение из 2 словаря
         z.update(x_1)
         z.update(y_1)
         print(z)
-    if key == 2:
+    if key == "б":
         for i, b in x_2.items():
             print(i, b)
-    if key == 3:
+    if key == "в":
         a = {}
-        for m in range(1, x_3 + 1):
-            k = list(input(f"список {m} "))
-            a.update(dict.fromkeys(str(m), k))
+        k = [[1,23,4,5],[121,131,441,15],[12,2],[1212]]
+        for m in k:
+            a.update(dict.fromkeys((str(k.index(m)+1)), m))
         print(a)
 
 
@@ -143,26 +143,23 @@ a = {"a":1,"b":2}
 b = {"c":3,"d":4}
 
 
-i_11(a,b,a,11, 3) #3 часть 11 задания
+#i_11(a,b,a,"в") #3 часть 11 задания #key это часть задания которую надо исполнить(русскими буквами)
 
-
-def i_12_a(x):
-    k_1 = 0
-    a = tuple()
-    for m in range(1, x + 1):
-        for k in range(1, int(input(f"колличество строк в словаре {m} ")) + 1):
-            k_1 += 1
-            a += (dict.fromkeys(str(k_1), input(f"{k} строка в словаре {m} ")),)
-    return a
-
-
-#print(i_12_a(int(input("колличество словарей "))))  #1 часть задания 12
-
+def i_12(x_1, x_2, key=""):
+    if key == "а":
+        k_1 = 0
+        a = tuple()
+        for m in range(1, x_1 + 1):
+            for k in range(1, int(input(f"количество строк в словаре {m} ")) + 1):
+                k_1 += 1
+                a += (dict.fromkeys(str(k_1), input(f"{k} строка в словаре {m} ")),)
+        print(a)
+    if key == "b":
+        print(tuple(sorted(x_2, key=lambda x: isinstance(x, float))))
 
 x = (21412, 214123.1, 142333, 12332.1231, 333333333333, 999999999999.1)
 
-
-#print(tuple(sorted(x, key=lambda x: isinstance(x, float)))) #2 часть задания 12
+#i_12(int(input("количество словарей: ")), x, "а")
 
 
 def i_13_a(x, y, z):
@@ -177,8 +174,11 @@ def i_13_a(x, y, z):
     else:
         return "не могут"
 
+i_1 = {1,2,3,4}#первая строка
+i_2 = {6,7,8,9}#вторая строка
+i_3 = {1,3,5}#строка с помощью которой мы проверяем
 
-#print(i_13_a({1,2,3,4,5},{6,7,8,9}, {3,5,10}))#1 часть 13 задания
+#print(i_13_a(i_1, i_2, i_3))#1 часть 13 задания
 
 
 def i_13_b(lst):
@@ -197,31 +197,31 @@ lst = [
     [
         {
             'id': 1,
-            'skils': ["1Ab", "2bC", "3"]
+            'skils': ["a", "B", "C"]
         }
     ],
     [
         {
             'id': 2,
-            'skils': ["1Ab", "2bC", "c"]
+            'skils': ["A", "B", "V"]
         }
     ],
     [
         {
             'id': 3,
-            'skils': ["1Ab", "2bC", "3c"]
+            'skils': ["A", "B", "X"]
         }
     ],
     [
         {
             'id': 4,
-            'skils': ["1Ab", "2bC", "C1"]
+            'skils': ["B", "A", "V"]
         }
     ],
 ]
 
 
-#print(i_13_b(lst))#2 часть 13 задания
+print(i_13_b(lst))#2 часть 13 задания
 
 
 def SumRange(a, b):
